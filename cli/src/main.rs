930dda75c20c917cd716b01a7686f07bdade73d0
rosafy <emmadomeisen@gmail.com>
2025-06-11 16:28:34 +0200

    Partially implemented solve fn

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,7 +1,7 @@
 use std::io;
 enum exprpartval {
-    num(f32),
-    oper(char),
+    num{value:f32},
+    oper{value:char},
 }
 struct exprpart {
     val: exprpartval,
@@ -35,7 +35,7 @@ fn eval(expr: String) -> f32 {
                     if active_num.is_empty() == false {
                         let anf32: f32 = active_num.clone().parse().unwrap();
                         let a_pos: i32 = pos;
-                        let a_exprpart = exprpart {val: exprpartval::num(anf32), pos: a_pos};
+                        let a_exprpart = exprpart {val: exprpartval::num{value:anf32}, pos: a_pos};
                         exprvec.push(a_exprpart);
                         active_num = String::new();
                         pos += 1;
@@ -45,7 +45,7 @@ fn eval(expr: String) -> f32 {
                     }
                     if !exprvec.is_empty() {
                         let a_pos: i32 = pos;
-                        exprvec.push(exprpart{val: exprpartval::oper(i),pos: a_pos});
+                        exprvec.push(exprpart{val: exprpartval::oper{value:i},pos: a_pos});
                         pos += 1;
                     }
 
@@ -53,17 +53,60 @@ fn eval(expr: String) -> f32 {
                     if active_num.is_empty() == false {
                         let anf32: f32 = active_num.clone().parse().unwrap();
                         let a_pos: i32 = pos;
-                        let a_exprpart = exprpart {val: exprpartval::num(anf32), pos: a_pos};
+                        let a_exprpart = exprpart {val: exprpartval::num{value:anf32}, pos: a_pos};
                         exprvec.push(a_exprpart);
                         active_num = String::new();
                         pos += 1;
                     } 
                     } else {
                         println!("Error: Illegal characters");
-                        return -1.0; 
+                        return f32::NAN; 
                     }
                 
             }
         }
+        for i in &exprvec {
+            match i.val {
+                exprpartval::num { value } => println!("Number: {}", value),
+                exprpartval::oper { value } => println!("Operator: {}", value),
+            }
+        } 
         return 0.0;
+}
+fn eval_solve(expr: Vec<exprpart>) -> Vec<exprpart>{
+    let mut rvec: Vec<exprpart> = Vec::new();
+    // Solve multiplications and divisions first
+    let mut to_remove: Vec<usize> = Vec::new();
+    for (i,val) in expr.iter().enumerate() {
+        match val.val {
+            exprpartval::num { value } => {},
+            exprpartval::oper { value } => {
+                let Operator = value;
+                if Operator == '*' {
+                    let mut lvalue = 0.0;
+                    let mut rvalue = 0.0;
+                    match expr[i-1].val {
+                        exprpartval::num {value} => {
+                            lvalue = value;
+                        },
+                        exprpartval::oper {value} => {},
+                    }
+                    match expr[i+1].val {
+                        exprpartval::num {value} => {
+                            rvalue = value;
+                        },
+                        exprpartval::oper {value} => {},
+                    }
+                    let solved = lvalue*rvalue;
+                    to_remove.push(i);
+                    to_remove.push(i-1);
+                    to_remove.push(i+1);
+                }
+            },
+        }
+    } 
+
+    return rvec;
+
+
 }
